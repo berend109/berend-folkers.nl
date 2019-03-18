869e9ac14a2f675fe4046f2af712b1ea49e2148d
berend109 <berend109@gmail.com>
2019-03-18 18:21:55 +0100

    made the files location more clear especially for inside the editor.

--- projects/forum/assets/php/utils/Connection.php
+++ projects/forum/assets/php/utils/Connection.php
@@ -1,14 +1,15 @@
 
 <?php
 
-    echo '<link rel="shortcut icon" href="../../../../../../../img/FaviconIMG.jpg">';
+    echo '<link rel="icon" href="../../img/FaviconIMG.jpg" type="image/gif" sizes="16x16">';
 
     $servername = 'localhost';
     $username = 'root';
     $password = '';
+    $dbname = "database";
 
     // Create connection
-    $conn = new mysqli($servername, $username, $password);
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
     // Check connection
     if ($conn->connect_error) {
