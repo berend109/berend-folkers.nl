88584279a1d5b6adce4ac38c102d77b84b73177a
berend109 <berend109@gmail.com>
2019-03-23 01:19:26 +0100

    included the conection.php file in the login and register page.

--- projects/forum/assets/php/Connection.php
+++ projects/forum/assets/php/Connection.php
@@ -1,26 +1,20 @@
-
 <?php
 
-    echo '<link rel="icon" href="../../img/FaviconIMG.jpg" type="image/gif" sizes="16x16">';
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'database';
 
-    $servername = 'localhost';
-    $username = 'root';
-    $password = '';
-    $dbname = "database";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    } // if successfully you get the message in screen and a link to go back to the main page.
-    else {
-        echo "Connection successfully";
-        echo "<br>";
-        echo "<br>";
-        echo "<br>";
-        echo "<a href='../../index.html'>Go back</a>"; 
-    };
-    
-?>
+// Check connection
+if ($conn->connect_error) {
+    die('Connection failed: ' . $conn->connect_error);
+} // if successfully you get the message in screen and a link to go back to the main page.
+else {
+    echo 'Connection successfully';
+    echo '<br>';
+    echo '<br>';
+    echo '<a href=\'../../index.html\'>Go back</a>';
+}
---
+++ projects/forum/assets/php/Login.php
@@ -0,0 +1,19 @@
+<?php
+    
+    require 'Connection.php';
+
+    $mail = $_POST['mail'];
+    $pswd = $_POST['pswd'];
+
+    echo '<br>';
+    echo '<br>';
+    echo 'mail = ';
+    echo $mail;
+    echo '<br>';
+    echo '<br>';
+    echo 'pswd = ';
+    echo $pswd;
+    echo '<br>';
+    echo '<br>';
+
+?>
