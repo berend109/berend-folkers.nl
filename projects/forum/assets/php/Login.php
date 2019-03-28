5ecfbfb7131e22415b10becc3895bdaf58ea0b09
berend109 <berend109@gmail.com>
2019-03-28 09:41:49 +0100

    backup start of day.

--- projects/forum/assets/php/Login.php
+++ projects/forum/assets/php/Login.php
@@ -16,12 +16,4 @@
     echo '<br>';
     echo '<br>';
 
-	// make conection
-	if ($conn->connect_error) {
-	    die('Connection failed: ' . $conn->connect_error);
-	} 
-	else {
-	    echo "string";
-	}
-
 ?>
--- projects/forum/assets/php/Register.php
+++ projects/forum/assets/php/Register.php
@@ -1,13 +1,6 @@
 <?php
     
     require 'Connection.php';
-
-    // make conection
-	if ($conn->connect_error) {
-	    die('Connection failed: ' . $conn->connect_error);
-	} 
-	else {
-	    echo "string";
-	}
+    
 
 ?>
