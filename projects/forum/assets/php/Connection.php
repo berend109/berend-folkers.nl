5421304326ff29e8898ef4772b50246744f83c97
berend109 <berend109@gmail.com>
2019-04-01 14:34:31 +0200

    a better way to connect to the DB

--- projects/forum/assets/php/Connection.php
+++ projects/forum/assets/php/Connection.php
@@ -8,12 +8,22 @@ class Connection {
     private $dbname;
     private $charset;
 
-    protected function connect() {
+    public function connect() {
         $this->servername = "localhost";
-        $this->username = "root";
+        $this->username = "roo";
         $this->password = "";
         $this->dbname = "database";
-        $this->charset = "utf8";
+        $this->charset = "utf8mb4";
+
+        try {
+            $dsn = "mysql:host=".$this->servername.";dbname=".$this->dbname.";charset=".$this->charset;
+            $pdo = new PDO($dsn, $this->username, $this->password);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $pdo;
+        } catch (PDOException $e) {
+            echo "Connection failed: ".$e->getMessage();
+        }
+        
     }
 
 }
--- projects/forum/assets/php/Register.php
+++ projects/forum/assets/php/Register.php
@@ -2,12 +2,17 @@
     
     require 'Connection.php';
     
-    $name = $_POST;
-
-
+    // test code
+    print_r(pdo_drivers());
+    echo "<br/>";
+    echo "<br/>";
     echo "usrname | pswd";
     echo "<br/>";
     echo "<br/>";
-    echo implode(" | ",$name);
+    echo implode(" | ",$_POST);
+    echo "<br/>";
+    echo "<br/>";
+    $object = new Connection;
+    $object->connect();
 
 ?>
