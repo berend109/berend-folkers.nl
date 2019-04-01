b56df72a40eda35a313af37f01cdcb267c7e0496
berend109 <berend109@gmail.com>
2019-04-01 22:33:26 +0200

    test

---
+++ projects/forum/assets/php/login.php
@@ -0,0 +1,12 @@
+<?php
+    
+    require 'connection.php';
+
+    echo "usrname | pswd";
+    echo "<br/>";
+    echo "<br/>";
+    echo implode(" | ",$_POST);
+    echo "<br/>";
+    echo "<br/>";
+    
+?>
---
+++ projects/forum/assets/php/register.php
@@ -0,0 +1,16 @@
+<?php
+
+    require 'connection.php';
+
+    // test code
+    // print_r(pdo_drivers());
+    // echo "<br/>";
+    // echo "<br/>";
+    echo "usrname | pswd";
+    echo "<br/>";
+    echo "<br/>";
+    echo implode(" | ",$_POST);
+    echo "<br/>";
+    echo "<br/>";
+
+?>
