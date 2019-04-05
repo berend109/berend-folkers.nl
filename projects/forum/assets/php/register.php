8c76423cca495f0d9b248094a577229a24c7cd73
berend109 <berend109@gmail.com>
2019-04-06 00:48:49 +0200

    test

--- projects/forum/assets/php/register.php
+++ projects/forum/assets/php/register.php
@@ -8,18 +8,9 @@
     // echo implode(" | ",$_POST);
     // echo "<br/>";
     // echo "<br/>"; 
-<<<<<<< HEAD
-    
-    class register extends connection {
-        public function userInfo() {
-            $stmt = $this->connect()->query("SELECT * FROM users");
-        }
-    }
-=======
 
 class register extends connection {
     public function userInfo() {
         $stmt = $this->connect()->query("SELECT * FROM users");
     }
 }
->>>>>>> 52fd95513ac99f4eaf0f84e87325b035df6f9f2a
