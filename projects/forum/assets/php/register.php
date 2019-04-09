2c6d2e12d580b7abe9b8a9d6779fb8090d8dea8f
berend109 <berend109@gmail.com>
2019-04-09 13:49:49 +0200

    made the register form working.

--- projects/forum/assets/php/register.php
+++ projects/forum/assets/php/register.php
@@ -5,24 +5,30 @@ require 'connection.php';
 $name = $_POST['username'];
 $pswd = $_POST['pswd'];
 
-class register extends connection {
+class Register {
+
+    public function __construct() {
+
+    }
 
     public function register($name, $pswd) {
 
+        try {
         $con = new connection;
         $connection = $con->connect();
 
-        $sql = "INSERT INTO 'persons' ('name', 'PSWD') VALUES ($name, $pswd)";
+        $sql = "INSERT INTO `persons` (`name`, `PSWD`) VALUES ('$name', '$pswd')";
 
-        if($connection->query($sql) === TRUE) {
-            echo "Je bent nu geregistreerd.";
-        } else {
-            echo "Error: " . $connection . "<br>" . $connection->error;
+        $stmt = $connection->prepare($sql);
+        $stmt->execute();
+
+        } catch (PDOException $e) {
+            echo "Something went wrong: " . $e->getMessage();
         }
 
     }
 
 }
 
-$reg = new register;
-$reg->register();
+$reg = new Register();
+$reg->register($name, $pswd);
