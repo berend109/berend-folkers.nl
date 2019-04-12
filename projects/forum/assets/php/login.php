6f083065c70b01fdfd18f70a29cd860c51c6a576
berend109 <berend109@gmail.com>
2019-04-12 18:07:48 +0200

    started with login.php

--- projects/forum/assets/php/login.php
+++ projects/forum/assets/php/login.php
@@ -3,14 +3,25 @@
 require 'connection.php';
 
 $name = $_POST['username'];
-$pswd = $_POST['pswd'];
-$pswd = md5($pswd);
+$pswd = md5($_POST['pswd']);
 
 class login extends connection {
-    
-    public function login() {
 
-        
+    public function __construct() {}
+    
+    public function login($name, $pswd) {
+
+        try {
+        $con = new connection;
+        $connection = $con->connect();
+
+        $sql = "SELECT `name`, `PSWD` FROM `persons` WHERE `name`=? AND `PSWD`=? ";
+        $query = $connection->prepare($sql);
+        $query->execute(array($name,$pswd));
+
+        } catch (PDOException $e) {
+            echo "Something went wrong: " . $e->getMessage();
+        }
 
     }
 
