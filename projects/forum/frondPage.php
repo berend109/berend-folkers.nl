6843899f659e80ab1271305fa7d5ac5fb3e7c808
berend109 <berend109@gmail.com>
2019-05-06 09:37:16 +0200

    test

--- projects/forum/frondPage.php
+++ projects/forum/frondPage.php
@@ -2,6 +2,12 @@
 
 session_start();
 
+if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+    echo "Welcome to the member's area, " . $_SESSION['username'] . "!";
+} else {
+    echo "Please log in first to see this page.";
+}
+
 ?>
 
 <!DOCTYPE html>
